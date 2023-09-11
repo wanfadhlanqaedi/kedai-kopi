ba1080318ded12f356534efb4b2871cd1f405e72
wan <135164021+wanfadhlanqaedi@users.noreply.github.com>
2023-09-11 13:00:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// toggle class active
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika menu di click
+
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// click di luar sidebar untuk menghilangkan nav
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
